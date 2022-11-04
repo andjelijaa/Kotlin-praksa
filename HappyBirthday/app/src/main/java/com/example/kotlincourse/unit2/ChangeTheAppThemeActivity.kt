9f439e9e75fe12cc09f41f93164368068837ffff
Andjelija Djordjevic <andjelija.djordjevic1997@gmail.com>
2022-11-04 16:20:23 +0100

    Unit2 with icon,style and themes

---
+++ HappyBirthday/app/src/main/java/com/example/kotlincourse/unit2/ChangeTheAppThemeActivity.kt
@@ -0,0 +1,12 @@
+package com.example.kotlincourse.unit2
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.kotlincourse.R
+
+class ChangeTheAppThemeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_changetheapptheme_main)
+    }
+}
